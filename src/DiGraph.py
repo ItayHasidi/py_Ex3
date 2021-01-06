dce52b1d02cfb46d5e95703f8a799d1bf6cc5941
Itay <ItayHasidi@gmail.com>
2021-01-06 16:11:47 +0200

    add_edge porb 1.0

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,47 +1,25 @@
 from GraphInterface import GraphInterface
 
 
-class Point:
-    def __init__(self, x: float, y: float, z: float):
-        self.x = x
-        self.y = y
-        self.z = z
-
-
-class Node:
-    tag = 0.0
-
-    def __init__(self, key: int, p: Point):
-        self.key = key
-        self.p = p
-
-
-class Edge:
-    def __init__(self, src: int, dest: int, weight: float):
-        self.weight = weight
-        self.src = src
-        self.dest = dest
-
-
 class DiGraph(GraphInterface):
     countMC = 0
     countE = 0
     countV = 0
-    nodeMap: dict
-    edgeMap: dict
-    edgeMapRev: dict
+    nodeMap: dict = {}
+    edgeMap: dict = {int: {int: float}}
+    edgeMapRev: dict = {int: {int: float}}
 
     def __init__(self):
         pass
 
     def v_size(self) -> int:
-        return DiGraph.countV
+        return self.countV
 
     def e_size(self) -> int:
-        return DiGraph.countE
+        return self.countE
 
     def get_mc(self) -> int:
-        return DiGraph.countMC
+        return self.countMC
 
     def get_all_v(self) -> dict:
         return self.nodeMap
@@ -50,25 +28,47 @@ class DiGraph(GraphInterface):
         return self.edgeMapRev[id1]
 
     def all_out_edges_of_node(self, id1: int) -> dict:
+        # i: dict
+        # temp: dict
+        # for i in self.edgeMap[id1]:
+        #     print(i)
+        #     if i
+
         return self.edgeMap[id1]
 
     def add_edge(self, id1: int, id2: int, weight: float) -> bool:
-        if id1 != id2 and id1 in self.nodeMap and id2 in self.nodeMap and id2 not in self.edgeMap[id1]:
-            e = Edge(id1, id2, weight)
-            self.edgeMap[id1][id2] = e
-            self.edgeMapRev[id2][id1] = e
-            self.countMC += 1
-            self.countE += 1
-            return True
+        if id1 is not id2:
+            if id1 in self.nodeMap:
+                if id2 in self.nodeMap:
+                    # if id2 not in self.edgeMap.get(id1):
+                    # e = Edge(id1, id2, weight)
+                    # temp: dict = {id2: weight}
+                    # temp2: dict = {id1: weight}
+                    d1: dict = self.edgeMap.get(id1)
+                    # d1.__setitem__({id2: weight})
+                    d1[id2] = weight
+                    print("id1")
+                    # print(id1)
+                    # print(id2)
+                    print(self.edgeMap.get(id1))
+                    self.edgeMapRev.get(id2).__setitem__({id1: weight})
+                    # self.edgeMap[id1], [id2] = e
+                    # self.edgeMapRev[id2], [id1] = e
+                    self.countMC += 1
+                    self.countE += 1
+                    return True
         return False
 
     def add_node(self, node_id: int, pos: tuple = None) -> bool:
-        p = Point(pos[0], pos[1], pos[2])
-        n = Node(node_id, p)
+        # if pos is not None:
+        #     p = Point(pos[0], pos[1], pos[2])
+        # else:
+        #     p = Point(0, 0, 0)
+        # n = Node(node_id, p)
         if node_id not in self.nodeMap:
             self.countMC += 1
             self.countV += 1
-            self.nodeMap[node_id] = n
+            self.nodeMap.__setitem__(node_id, pos)
             return True
         return False
 
@@ -89,7 +89,8 @@ class DiGraph(GraphInterface):
         return False
 
     def remove_edge(self, node_id1: int, node_id2: int) -> bool:
-        if node_id1 in self.edgeMap and node_id2 in self.edgeMapRev:
+        if node_id1 in self.edgeMap and node_id2 in self.edgeMap[node_id1] \
+                and node_id2 in self.edgeMapRev and node_id1 in self.edgeMapRev[node_id2]:
             del self.edgeMap[node_id1][node_id2]
             del self.edgeMapRev[node_id2][node_id1]
             self.countMC += 1
